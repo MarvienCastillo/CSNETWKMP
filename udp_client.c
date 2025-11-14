9cd3a7ff758dff37921890eeb9dbd094691d0e19
Marvien Castillo <160254067+MarvienCastillo@users.noreply.github.com>
2025-11-15 02:38:39 +0800

    a

--- udp_client.c
+++ udp_client.c
@@ -3,11 +3,22 @@
 #include <string.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
-
+#define MaxBufferSize 1024
 #pragma comment(lib,"ws2_32.lib")
-
+// returns the string after the message_type 
+char *get_message_type(char *message){
+    char *message_type = strstr(message,"message_type: ");
+    if(message_type){
+        return message_type + 14;
+    }
+    return NULL;
+}
 int main(){
     WSADATA wsa;
+    SOCKADDR_IN SenderAddr;
+    int SenderAddrSize = sizeof(SenderAddr);
+
+    int TotalbyteSent;
     // start winsock
      if (WSAStartup(MAKEWORD(2,2), &wsa) != 0) {
         printf("Failed! Error code: %d\n", WSAGetLastError());
@@ -26,13 +37,71 @@ int main(){
 
     //specify an address for the socket
     struct sockaddr_in server_address;
+    struct sockaddr_in from_server; // seperate address for receiving message from the server
+    int add_len = sizeof(from_server);
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(9002);
     server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
-    if (connect(socket_network, (struct sockaddr*)&server_address, sizeof(server_address)) == SOCKET_ERROR) {
-        printf("Connection failed! Error code: %d\n", WSAGetLastError());
-        closesocket(socket_network);
-        WSACleanup();
-        return 1;
-    }
+    char buffer[MaxBufferSize]; 
+    char receive[MaxBufferSize];
+    int byte_received;
+    int message_len;
+    int seed;
+    do{
+        // This checks if the client received a message from the server
+        fd_set read; 
+        struct timeval timeout;
+
+        FD_ZERO(&read);
+        FD_SET(socket_network,&read);
+
+        timeout.tv_sec = 1;
+        timeout.tv_usec = 1;
+        
+        int activity = select(0,&read,NULL,NULL,&timeout);
+        if(activity>0){
+            byte_received = recvfrom(socket_network,receive,sizeof(receive),0,(SOCKADDR *)&from_server,&add_len);
+            if(byte_received > 0){
+                receive[byte_received] = '\0';
+                char *message = get_message_type(receive);
+                if(strncmp(message,"HANDSHAKE_RESPONSE",strlen("HANDSHAKE_RESPONSE")) == 0)
+                {
+                    char *seed_ptr = strstr(message,"seed:");
+                    if(seed_ptr != NULL){
+                        int scanStatus = sscanf(seed_ptr,"seed: %d", &seed);
+                        if (scanStatus == 1){
+                            printf("Seed is %d", seed);
+                        }
+                    }
+                    printf("\nClient: Received response/update: %s\n", message);
+                }
+
+                continue;
+            }
+        }
+
+        // User Input
+        printf("\nmessage_type: ");
+        if (fgets(buffer, MaxBufferSize, stdin) == NULL) {
+            break; // Exit loop on error
+        }
+        // Remove newline character if present
+        message_len = strlen(buffer);
+        if (message_len > 0 && buffer[message_len - 1] == '\n') {
+            buffer[message_len - 1] = '\0';
+            message_len--; // Adjust length to exclude the null terminator for sending
+        }
+
+        // Add the message_type: prefix for compliance (basic version)
+        char full_message[MaxBufferSize + 16]; 
+        sprintf(full_message, "message_type: %s", buffer);
+        
+        printf("Client: Data to be sent: %s\n", full_message);
+        printf("Sending data....\n");
+        
+        message_len = strlen(full_message);
+        sendto(socket_network,full_message,message_len,0,(SOCKADDR *)&server_address,sizeof(server_address));
+    } while(1);
+    closesocket(socket_network);
+    WSACleanup();
 }
--- udp_server.c
+++ udp_server.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
-
+#include <string.h>
+#define MaxBufferSize 1024
 #pragma comment(lib,"ws2_32.lib")
-
+// returns the string after the message_type 
+char *get_message_type(char *message){
+    char *message_type = strstr(message,"message_type: ");
+    if(message_type){
+        return message_type + 14;
+    }
+    return NULL;
+}
 int main(){
     WSADATA wsa;
-
+    char receive[MaxBufferSize];
+    SOCKADDR_IN SenderAddr;
+    int SenderAddrSize = sizeof(SenderAddr);
+    int ByteReceived=0;
+    char response[MaxBufferSize];
     //start Winsock
     if (WSAStartup(MAKEWORD(2,2), &wsa) != 0) {
         printf("Failed! Error code: %d\n", WSAGetLastError());
-        return 1;
-    }
-
-    //create server socket
-    SOCKET server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket == INVALID_SOCKET) {
-        printf("Could not create socket: %d\n", WSAGetLastError());
-        WSACleanup();
         return 1;
     }
 
@@ -29,24 +34,72 @@ int main(){
     server_address.sin_port = htons(9002);
     server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    //bind
-    if (bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address)) == SOCKET_ERROR) {
-        printf("Bind failed: %d\n", WSAGetLastError());
-        closesocket(server_socket);
-        WSACleanup();
-        return 1;
+    SOCKET receiving_socket = socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP);
+    if (receiving_socket == INVALID_SOCKET){
+
+          //Print error message
+          printf("Server: Error at socket(): %ld\n", WSAGetLastError());
+
+          // Clean up
+          WSACleanup();
+
+          // Exit with error
+          return -1;
+     }
+    else{
+        printf("Server: socket() is OK!\n");
     }
 
-    //listen
-    listen(server_socket, 5);
-    printf("Server listening on port 9002...\n");
+    if(bind(receiving_socket,(SOCKADDR *)&server_address,sizeof(server_address)) == SOCKET_ERROR){
+        // Print error message
+        printf("Server: Error! bind() failed!\n");
 
-    //accept
-    SOCKET client_socket = accept(server_socket, NULL, NULL);
-    if (client_socket == INVALID_SOCKET) {
-        printf("Accept failed: %d\n", WSAGetLastError());
-        closesocket(server_socket);
+        // Close the socket
+        closesocket(receiving_socket);
+
+        // Do the clean up
         WSACleanup();
-        return 1;
+
+        // and exit with error
+        return -1;
     }
+    else{
+        printf("Server: bind() is OK!\n");
+    }
+
+    // Print some info on the receiver(Server) side...
+    getsockname(receiving_socket, (SOCKADDR *)&server_address, (int *)sizeof(receiving_socket));
+    printf("Server: Receiving IP(s) used: %s\n", inet_ntoa(server_address.sin_addr));
+
+    printf("Server: Receiving port used: %d\n", htons(server_address.sin_port));
+
+    printf("Server: I\'m ready to receive data packages. Waiting...\n\n");
+    bool flag = true;
+    do{ 
+        // Ensure the receive/response buffer is cleared before each read
+        memset(receive,0,MaxBufferSize);
+        memset(response,0,MaxBufferSize);
+        ByteReceived = recvfrom(receiving_socket,receive, sizeof(receive), 0, (SOCKADDR *)&SenderAddr, &SenderAddrSize);
+        char *message = get_message_type(receive);
+        if(message){
+            if(strncmp(message,"exit",4) == 0){
+                printf("Server exiting\n");
+                flag = false;
+            }
+            else if(strcmp(message,"HANDSHAKE_REQUEST") == 0){
+                printf("Server: Handshake Request has been received\n");
+                int seed = 12345;
+                sprintf(response,"message_type: HANDSHAKE_RESPONSE\nseed: %d", seed);
+                sendto(receiving_socket,response,strlen(response),0,(SOCKADDR *)&SenderAddr,SenderAddrSize);
+                printf("A Handshake response has been sent with a seed of %d", seed);
+            }   
+            else {
+                printf("Server: Received %d bytes\n", ByteReceived);
+                printf("Server: The data is: %s\n", receive);
+                printf("\n");
+            }
+        }
+   } while(flag);
+   closesocket(receiving_socket);
+   WSACleanup();
 }
